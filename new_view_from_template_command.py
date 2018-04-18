e2036482d7c9f6251af147267264ad0742eface0
thom <thom@localhost.localdomain>
2018-04-18 12:51:35 -0400

    Updated for new_view API.

--- new_view_from_template_command.py
+++ new_view_from_template_command.py
@@ -3,7 +3,8 @@ import sublime_plugin
 
 import os.path
 
-from sublime_lib.util import create_view
+from sublime_lib.view_utils import new_view
+from sublime_lib.syntax import projection
 
 import yaml
 
@@ -12,12 +13,12 @@ def load_template(path):
 
     i = template.find('\n---\n')
     ret = yaml.load(template[:i])
-    text = template[i+5:]
+    contents = template[i+5:]
 
-    if text[-1] == '\n':
-        text = text[:-1]
+    if contents[-1] == '\n':
+        contents = contents[:-1]
 
-    ret['text'] = text
+    ret['contents'] = contents
     ret.setdefault('name', os.path.splitext(os.path.basename(path))[0])
     ret.setdefault('description', path)
 
@@ -32,9 +33,11 @@ class NewViewFromTemplateCommand(sublime_plugin.WindowCommand):
 
         def callback(i):
             if i == -1: return
+            template = templates[i]
             self.create_view_from_template(
-                text=templates[i]['text'],
-                settings=templates[i].get('settings', None)
+                **projection(template, {
+                    'settings', 'syntax', 'scope', 'contents'
+                })
             )
 
         self.window.show_quick_panel(
@@ -45,6 +48,6 @@ class NewViewFromTemplateCommand(sublime_plugin.WindowCommand):
             callback
         )
 
-    def create_view_from_template(self, *, text, settings=None):
-        view = create_view(self.window, settings=settings)
-        view.run_command('insert_snippet', {'contents': text})
+    def create_view_from_template(self, *, contents, **kwargs):
+        view = new_view(self.window, **kwargs)
+        view.run_command('insert_snippet', {'contents': contents})
