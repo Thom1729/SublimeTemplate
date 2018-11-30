0150ed4fb89ab7d5173d536cbf490824cc04749a
thom <thom@localhost.localdomain>
2018-11-30 14:10:59 -0500

    Did things.

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # SublimeTemplate
 A simple, powerful file template system for Sublime Text.
+
+## Usage
+
+Press <kbd>ctrl</kbd><kbd>shift</kbd><kbd>M</kbd> (on Mac, <kbd>⌘</kbd><kbd>shift</kbd><kbd>M</kbd>) to open the template selector.
+
+## Creating a template
+
+A template is a file with the `.sublime-template` extension that resembles the following:
+
+```
+description: For demonstration purposes.
+scope: source.markdown
+---
+# Hello, World!
+
+This is an example of a sublime-template!
+```
+
+Templates have two sections, separated by a line containing only `---`. The first section contains information about the template (in YAML). The second section is the body of the template, containing any text you would like. You can use snippet placeholders in the body.
+
+### Template metadata
+
+#### Name
+
+The name of the template as it will appear in the template selector. If omitted, defaults to the name of the template file (minus the `.sublime-template` extension).
+
+#### Description
+
+The description that will appear in the template selector below the name. If omitted, defaults to the path of the template.
+
+#### Scope
+
+If specified, the new file will use the usual syntax definition for that scope. Cannot be combined with the `syntax` option.
+
+#### Syntax
+
+The resource path of a syntax definition to use for the new file. Cannot be combined with the `scope` option.
+
+#### Settings
+
+A mapping of settings to set on the new view.
--- new_view_from_template_command.py
+++ new_view_from_template_command.py
@@ -1,43 +1,89 @@
 import sublime
 import sublime_plugin
 
-import os.path
+from sublime_lib.view_utils import new_view, close_view
+from sublime_lib import show_selection_panel, ResourcePath, NamedSettingsDict
 
-from sublime_lib.view_utils import new_view
-from sublime_lib.collection_utils import projection
-from sublime_lib import show_selection_panel
+from collections import namedtuple
 
 import yaml
 
-def load_template(path):
-    template = sublime.load_resource(path)
 
-    i = template.find('\n---\n')
-    ret = yaml.load(template[:i])
-    contents = template[i+5:]
+TemplateDefinition = namedtuple(
+    'TemplateDefinition',
+    ['path', 'metadata', 'body']
+)
 
-    if contents[-1] == '\n':
-        contents = contents[:-1]
 
-    ret['contents'] = contents
-    ret.setdefault('name', os.path.splitext(os.path.basename(path))[0])
-    ret.setdefault('description', path)
+def plugin_loaded():
+    global SETTINGS
+    SETTINGS = NamedSettingsDict("SublimeTemplate")
 
-    return ret
+
+import re
+EXPR = re.compile(r'^---$', re.MULTILINE)
+def parse_yaml_body(text):
+    i = EXPR.search(text).start()
+    yaml_part = text[:i]
+    body_part = text[i+4:]
+
+    if len(body_part) and body_part[-1] == '\n':
+        body_part = body_part[:-1]
+
+    return (yaml.load(yaml_part) or {}, body_part)
+
+
+def get_templates():
+    return [
+        TemplateDefinition(path, *parse_yaml_body(path.read_text()))
+        for path in ResourcePath.glob_resources('*.sublime-template')
+    ]
+
 
 class NewViewFromTemplateCommand(sublime_plugin.WindowCommand):
-    def run(self):
+    preview = None
+
+    def run(self, context={}):
+        self.context = context
         show_selection_panel(
             self.window,
-            [load_template(path) for path in sublime.find_resources('*.sublime-template')],
-            labels=lambda template: (template['name'], template['description']),
-            on_select=lambda template: self.create_view_from_template(
-                **projection(template, {
-                    'settings', 'syntax', 'scope', 'contents'
-                })
-            )
+            get_templates(),
+
+            labels=lambda template: (
+                template.metadata.get('name', template.path.stem),
+                template.metadata.get('description', template.path),
+            ),
+
+            on_select=self.on_select,
+            on_cancel=self.on_cancel,
+            on_highlight=self.on_highlight if SETTINGS['preview_on_highlight'] else None,
         )
 
-    def create_view_from_template(self, *, contents, **kwargs):
+    def on_highlight(self, template):
+        if self.preview:
+            close_view(self.preview, force=True)
+
+        self.preview = self.create_view_from_template(template)
+
+    def on_cancel(self):
+        if self.preview:
+            close_view(self.preview, force=True)
+
+        self.preview = None
+
+    def on_select(self, template):
+        if not self.preview:
+            self.create_view_from_template(template)
+
+        self.preview = None
+
+    def create_view_from_template(self, template):
+        kwargs = { k: v for k, v in template.metadata.items() if k in {'settings', 'syntax', 'scope'} }
+
         view = new_view(self.window, **kwargs)
-        view.run_command('insert_snippet', {'contents': contents})
+
+        args = {'contents': template.body}
+        args.update(self.context)
+        view.run_command('insert_snippet', args)
+
+        return view
